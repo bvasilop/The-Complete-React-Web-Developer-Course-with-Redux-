3acb9ee669008bd5a9b3aa924a78c6c2ea362812
bvasilop <bvasilop@gmail.com>
2018-11-07 13:52:41 -0800

    added eslint and app.js components

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -8,11 +8,68 @@ console.log('app.js is running!');
 // JSX = JavaScript XML (JavaScript syntax extension)
 
 var template = React.createElement(
-  'h1',
-  { id: 'someId' },
-  'Indecision App!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        { id: 'someId' },
+        'Indecision App!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some text'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item three'
+        )
+    )
 );
 
-var appRoot = document.getElementById('app');
+var appRoot1 = document.getElementById('app1');
 
-ReactDOM.render(template, appRoot); // we use react render  to render this template in this element in the browser.
+ReactDOM.render(template, appRoot1); // we use react render  to render this template in this element in the browser.
+
+// Create a templateTwo var JSX expression
+// root div
+// h1 -> your name
+// p -> Age: 43
+// p -> Location: Seattle
+// Render templateTwo instead of template
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Bill Vasilopoulos'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age : 43'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location : Seattle'
+    )
+);
+var appRoot2 = document.getElementById('app2');
+ReactDOM.render(templateTwo, appRoot2);
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -5,9 +5,38 @@ console.log('app.js is running!');
 
 // JSX = JavaScript XML (JavaScript syntax extension)
 
-var template = <h1 id="someId">Indecision App!</h1>;
+var template = (
+    <div>
+		<h1 id='someId'>Indecision App!</h1>
+		<p>This is some text</p>
+		<ol>
+			<li>Item one</li>
+			<li>Item two</li>
+			<li>Item three</li>
+		</ol>
+    </div>
+);
 
 
-var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot); // we use react render  to render this template in this element in the browser.
+var appRoot1 = document.getElementById('app1');
+
+
+ReactDOM.render(template, appRoot1); // we use react render  to render this template in this element in the browser.
+
+// Create a templateTwo var JSX expression
+    // root div
+        // h1 -> your name
+        // p -> Age: 43
+        // p -> Location: Seattle
+        // Render templateTwo instead of template
+
+var templateTwo = (
+    <div>
+        <h1>Bill Vasilopoulos</h1>
+        <p>Age : 43</p>
+        <p>Location : Seattle</p>
+    </div>
+);
+var appRoot2 = document.getElementById('app2');
+ReactDOM.render(templateTwo, appRoot2);
