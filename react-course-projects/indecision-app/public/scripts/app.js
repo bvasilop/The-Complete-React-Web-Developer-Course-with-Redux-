83f3ce4a3a1dc36d2f54b16ce279db1e09a5183b
bvasilop <bvasilop@gmail.com>
2018-11-10 21:47:22 -0800

    added content and onClick events to app.js

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -1,51 +1,85 @@
 'use strict';
 
-// arguments object - no longer bound
-// this keyword - no longer bound
+// contains our jsx content for the app
+console.log('app.js is running!');
 
-var add = function add(a, b) {
-	console.log(arguments); // access to arguments object
-	return a + b;
-};
-console.log(add(55, 1001, 99)); // third argument is still accessible when console.log above
-// we have access to argument objects in es5 but not in es6 arrow functions
+var template = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		{ id: 'someId' },
+		'Indecision App!'
+	),
+	React.createElement(
+		'p',
+		null,
+		'This is some text'
+	),
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			'Item one'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item two'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item three'
+		)
+	)
+);
 
-var add2 = function add2(a, b) {
+var count = 0;
 
-	return a + b;
-};
-console.log(add2(5, 1, 9));
-
-var user = {
-	name: 'Bill',
-	cities: ['Seattle', 'Chicago', 'Bloomington'],
-	// printPlacesLived: function () { // don't use arrow function in methods because you lose access to this.cities unless using es6 methods syntax
-	printPlacesLived: function printPlacesLived() {
-		var _this = this;
-
-		// es 6 method syntax
-		return this.cities.map(function (city) {
-			return _this.name + ' has lived in ' + city + '!';
-		});
-	}
-};
-console.log(user.printPlacesLived());
-// make an object
-// use a method
-
-var multiplier = {
-	// numbers - array of numbers
-	// multiply by - single number
-	// multiply method that returns new array where numbers have been multiply
-	numbers: [2, 4, 6],
-	multiplyBy: 3,
-	multiply: function multiply() {
-		var _this2 = this;
-
-		return this.numbers.map(function (number) {
-			return number * _this2.multiplyBy;
-		});
-	}
+var addOne = function addOne() {
+	console.log('addOne');
 };
 
-console.log(multiplier.multiply());
+var minusOne = function minusOne() {
+	console.log('minusOne');
+};
+
+var reset = function reset() {
+	console.log('reset');
+};
+
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'Count: ',
+		count
+	),
+	React.createElement(
+		'button',
+		{ onClick: addOne },
+		'+1'
+	),
+	React.createElement(
+		'button',
+		{ onClick: minusOne },
+		'-1'
+	),
+	React.createElement(
+		'button',
+		{ onClick: reset },
+		'reset'
+	)
+);
+console.log(templateTwo);
+
+// Challenge
+// button with text "-1" --setup minusOne function and register - log "minusOne"
+// make reset button "reset" - setup reset function -log "reset"
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,31 +1,11 @@
 // contains our jsx content for the app
 console.log('app.js is running!');
 
-/*** auto generated file that will contain our babel transformations */
-
-// JSX = JavaScript XML (JavaScript syntax extension)
-
-// only render the subtitle (and p tag) if subtitle exists - logical and operator
-// render new p tag - if options.length is > 0 then "Here are your options" else  "No options"
-const app = {
-    title: 'Star Wars',
-		subtitle: 'Return of the Jedi',
-		options: ['One, Two']
-};
-
-const templateOne = (
-    <div>
-      <h1>Title : {app.title}</h1>
-      {app.subtitle && <p>Subtitle : {app.subtitle}</p>}
-			<p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-		</div>
-);
-
-const appRoot1 = document.getElementById('app1');
-ReactDOM.render(templateOne, appRoot1);
 
 
-const templateTwo = (
+
+
+const template = (
     <div>
 		<h1 id='someId'>Indecision App!</h1>
 		<p>This is some text</p>
@@ -37,42 +17,36 @@ const templateTwo = (
     </div>
 );
 
-const appRoot2 = document.getElementById('app2');
-ReactDOM.render(templateTwo, appRoot2); // we use react render  to render this template in this element in the browser.
+let count = 0;
 
-// Create a templateTwo var JSX expression
-    // root div
-        // h1 -> your name
-        // p -> Age: 43
-        // p -> Location: Seattle
-        // Render templateTwo instead of template
-const user = {
-    name: 'Bill Vasilopoulos',
-    age: 43,
-    location: 'Seattle'
+const addOne = () => {
+console.log('addOne');
 };
 
-function getLocation(location) {
-	if (location) {
-		return <p>Location: {location} </p>;
-	}
-}
+const minusOne = () => {
+	console.log('minusOne');
+};
 
+const reset = () => {
+	console.log('reset');
+};
 
-const templateThree = (
-    <div>
-      <h1>Name: {user.name ? user.name : 'Anonymous'}</h1>
-      {(user.age && user.age >= 18) && <p>Age : {user.age}</p>}
-			{getLocation(user.location)}
-    </div>
+const templateTwo = (
+	<div>
+		<h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+		<button onClick={minusOne}>-1</button>
+		<button onClick={reset}>reset</button>
+	</div>
 );
-const appRoot3 = document.getElementById('app3');
-ReactDOM.render(templateThree, appRoot3);
+console.log(templateTwo);
+
+// Challenge
+// button with text "-1" --setup minusOne function and register - log "minusOne"
+// make reset button "reset" - setup reset function -log "reset"
+const appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
 
 
-
-// if statements
-// ternary operator
-// logical && operator
 
 
