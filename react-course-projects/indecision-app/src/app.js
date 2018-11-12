1bb2cc051d6e1486f2d9195a2c6e4cddf1301f1f
bvasilop <bvasilop@gmail.com>
2018-11-11 16:28:55 -0800

    completed counter components

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,10 +1,6 @@
 // contains our jsx content for the app
 console.log('app.js is running!');
 
-
-
-
-
 const template = (
     <div>
 		<h1 id='someId'>Indecision App!</h1>
@@ -20,33 +16,35 @@ const template = (
 let count = 0;
 
 const addOne = () => {
-console.log('addOne');
+	count++;
+renderCounterApp();
 };
 
 const minusOne = () => {
-	console.log('minusOne');
+	count--;
+	renderCounterApp();
 };
 
 const reset = () => {
-	console.log('reset');
+	count = 0;
+	renderCounterApp();
 };
 
-const templateTwo = (
-	<div>
-		<h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-		<button onClick={minusOne}>-1</button>
-		<button onClick={reset}>reset</button>
-	</div>
-);
-console.log(templateTwo);
-
-// Challenge
-// button with text "-1" --setup minusOne function and register - log "minusOne"
-// make reset button "reset" - setup reset function -log "reset"
 const appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
 
+const renderCounterApp = () => {
+	const templateTwo = (
+		<div>
+			<h1>Count: {count}</h1>
+			<button onClick={addOne}>+1</button>
+			<button onClick={minusOne}>-1</button>
+			<button onClick={reset}>reset</button>
+		</div>
+	);
+	ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
 
 
 
