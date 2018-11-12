95be33a75381866cbf3e01eb91e848d86f2de625
bvasilop <bvasilop@gmail.com>
2018-11-11 21:38:57 -0800

    added forms content to app.js

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -6,90 +6,89 @@ console.log('app.js is running!');
 var app = {
 	title: 'Indecision App',
 	subtitle: 'Put your life in the hands of a computer',
-	options: ['One, Two']
+	options: []
 };
 
-var template = React.createElement(
-	'div',
-	null,
-	React.createElement(
-		'h1',
-		null,
-		app.title
-	),
-	app.subtitle && React.createElement(
-		'p',
-		null,
-		'Subtitle : ',
-		app.subtitle
-	),
-	React.createElement(
-		'p',
-		null,
-		app.options.length > 0 ? 'Here are your options' : 'No options'
-	),
-	React.createElement(
-		'ol',
-		null,
-		React.createElement(
-			'li',
-			null,
-			'Item one'
-		),
-		React.createElement(
-			'li',
-			null,
-			'Item two'
-		)
-	)
-);
+var onFormSubmit = function onFormSubmit(e) {
+	// e is event object
+	e.preventDefault(); // stops full page refresh and runs code for event
+	// console.log('Form submitted');
+	var option = e.target.elements.option.value;
 
-var count = 0;
-
-var addOne = function addOne() {
-	count++;
-	renderCounterApp();
+	if (option) {
+		app.options.push(option); // push on new array
+		e.target.elements.option.value = ''; // set value to an empty string
+		// add function for rendering data
+		renderFormApp(); // called after options array has been changed
+	}
 };
-
-var minusOne = function minusOne() {
-	count--;
-	renderCounterApp();
-};
-
-var reset = function reset() {
-	count = 0;
-	renderCounterApp();
+var onRemoveAll = function onRemoveAll() {
+	app.options = []; // set to new empty array (clears out all items)
+	renderFormApp();
 };
 
 var appRoot = document.getElementById('app');
+// create remove all button
+// onCLick handler which will wipe app.options setting it equal to an empty array(re-render the app)
+// count goes back to zero
 
-var renderCounterApp = function renderCounterApp() {
-	var templateTwo = React.createElement(
+
+// const numbers = [55, 101, 1000];
+var renderFormApp = function renderFormApp() {
+	var template = React.createElement(
 		'div',
 		null,
 		React.createElement(
 			'h1',
 			null,
-			'Count: ',
-			count
+			app.title
+		),
+		app.subtitle && React.createElement(
+			'p',
+			null,
+			'Subtitle : ',
+			app.subtitle
+		),
+		React.createElement(
+			'p',
+			null,
+			app.options.length > 0 ? 'Here are your options' : 'No options'
+		),
+		React.createElement(
+			'p',
+			null,
+			app.options.length
 		),
 		React.createElement(
 			'button',
-			{ onClick: addOne },
-			'+1'
+			{ onClick: onRemoveAll },
+			'Remove all'
 		),
 		React.createElement(
-			'button',
-			{ onClick: minusOne },
-			'-1'
+			'ol',
+			null,
+			app.options.map(function (option) {
+				return React.createElement(
+					'li',
+					{ key: option },
+					option
+				);
+			})
 		),
 		React.createElement(
-			'button',
-			{ onClick: reset },
-			'reset'
+			'form',
+			{ onSubmit: onFormSubmit },
+			React.createElement('input', { type: 'text', name: 'option' }),
+			React.createElement(
+				'button',
+				null,
+				'Add option'
+			)
 		)
 	);
-	ReactDOM.render(templateTwo, appRoot);
+
+	ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+// initialize function
+renderFormApp();
