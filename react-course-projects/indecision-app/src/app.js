36f65de9e96094d533a85713502ab9fea75a59b5
bvasilop <bvasilop@gmail.com>
2019-02-17 15:22:48 -0800

    working version with New class syntax capabilities

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -4,7 +4,19 @@ import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
+class OldSyntax {
+	constructor() {
+		this.name = 'Bill';
+	}
+}
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
 
+class NewSyntax {
+
+}
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
 
 
 
