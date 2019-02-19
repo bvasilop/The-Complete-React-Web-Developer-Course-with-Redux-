d80723d81fcc1c122bb4401dca25ee427e85f280
bvasilop <bvasilop@gmail.com>
2019-02-18 20:43:18 -0800

    added variables for background and container sizing and colors

--- react-course-projects/indecision-app/src/components/IndecisionApp.js
+++ react-course-projects/indecision-app/src/components/IndecisionApp.js
@@ -81,6 +81,7 @@ export default class IndecisionApp extends React.Component {
 		return (
 			<div>
 				<Header subtitle={subtitle} />
+				<div className="container">
 				<Action
 					hasOptions={this.state.options.length > 0}
 					handlePick={this.handlePick}
@@ -93,6 +94,7 @@ export default class IndecisionApp extends React.Component {
 				<AddOption
 					handleAddOption={this.handleAddOption}
 				/>
+				</div>
 				<OptionModal
 					selectedOption={this.state.selectedOption}
 					handleClearSelectedOption={this.handleClearSelectedOption}
